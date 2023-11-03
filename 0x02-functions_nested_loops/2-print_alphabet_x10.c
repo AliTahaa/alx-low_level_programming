d3b0ece3ca79353feedb1871b57caa3e38d69e45
AliTahaa <ali.mohamed.taha.h@gmail.com>
2023-11-03 22:18:29 +0200

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,20 +6,13 @@
  */
 void print_alphabet_x10(void)
 {
-	char ch;
-	int i;
+	char letter, i;
 
-	i = 0;
-
-	while (i < 10)
+	for (i = 0; i < 10; i++)
 	{
-		ch = 'a';
-		while (ch <= 'z')
-		{
-			_putchar(ch);
-			ch++;
-		}
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
 		_putchar('\n');
-		i++;
 	}
+
 }
