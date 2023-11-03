069aa375dfc3f51f85f6c55ddbcace51782fe136
AliTahaa <ali.mohamed.taha.h@gmail.com>
2023-11-03 23:23:03 +0200

    task 11 fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (i = n; i < 98; i++)
 		{
 			printf("%d, ", i);
 		}
@@ -20,7 +20,7 @@ void print_to_98(int n)
 	}
 	else if (n >= 98)
 	{
-		for (j = n; j >= 98; j--)
+		for (j = n; j > 98; j--)
 		{
 			printf("%d, ", j);
 		}
