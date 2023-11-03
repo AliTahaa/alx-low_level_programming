2b23b804fcac822280c291f98f0d74ca6cb418a6
AliTahaa <ali.mohamed.taha.h@gmail.com>
2023-11-03 14:16:57 +0200

    fix task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
 /**
  * main - print _putchar
- * is written by mbah
  * Return: 0
  */
 
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+	printf("_putchar\n");
 	return (0);
 }
